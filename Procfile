5068cef8252395b5c72175e90a650f15e203f956
abemorcardc <abemorcar@alum.us.es>
2021-02-12 10:41:58 +0000

    <heroku> Configuracion para desplegar en heroku
    Configuracion en settings.py y añadido procfile para desplegar en heroku
    
    <tarea: #>

---
+++ Procfile
@@ -0,0 +1,5 @@
+ra el repositorio para su despliegue. 
+release: sh -c 'cd decide && python manage.py migrate'
+% especifica el comando para lanzar Decide
+web: sh -c 'cd decide && gunicorn decide.wsgi --log-file -'
+
--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -163,10 +163,12 @@ STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
+APIS={}
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
+BASE_URL="https://egc-1230-abemorcar.herokuapp.com"
 
 try:
     from local_settings import *
@@ -183,3 +185,5 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+import django_heroku
+django_heroku.settings(locals())
